6dc6abb8148268c35543537b509bb4676cbff232
Matthew Robinson <robinson.me@gmail.com>
2017-11-02 22:22:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  var hiddenFunction = function() {
+  return function() {
     return "FUNKY!"
   }
-  return hiddenFunction
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
